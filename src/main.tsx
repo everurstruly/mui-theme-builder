76d49c83c86a01f08bdb7786ac28df46781efc35
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-18 19:19:19 +0100

    feat: create a landing page from mui template

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "./global.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import HomePage from "./page";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <HomePage />
   </StrictMode>
 );
