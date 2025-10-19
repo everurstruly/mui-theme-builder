007b74f77b2b6bb3a85a8e331f762f0f225fe71e
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-20 00:20:19 +0100

    feat: implement routing for HomePage and EditorPage

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,19 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./global.css";
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./page";
+import EditorPage from "./editor/page";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HomePage />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/editor" element={<EditorPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
