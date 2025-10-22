2409bd09e5cab310c0bd3ddf93724d4a88c91ed1
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-22 22:37:34 +0100

    feat: centralize visual state and allow a responsive layout

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -1,46 +1,77 @@
-import EditorActivitiesPanel from "./ActivitiesPanel";
 import EditorToolBar from "./ToolBar";
 import EditorCanvas from "./Canvas";
 import EditorPropertiesPanel from "./PropertiesPanel";
 import EditorPropertiesDrawerToggle from "./PropertiesPanel/DrawerToggle";
-import EditorMobileHeader from "./MobileHeader";
-import useEditorStore from "./Editor.store";
-import { Stack } from "@mui/material";
-import { headerHeightCss } from "./constants";
-import { editorPropertiesPanelWidth } from "./PropertiesPanel/constants";
+import EditorHeader from "./Header";
+import EditorPreviewsPanel from "./PreviewsPanel";
+import { Box, Stack } from "@mui/material";
+import useResponsivePanelsPlacements from "./useResponsivePanelsPlacements";
 
 export default function Editor() {
-  const isPropertiesPanelVisible = useEditorStore((state) => {
-    return !state.hiddenPanels.includes("properties");
-  });
+  useResponsivePanelsPlacements();
 
   return (
-    <>
-      <EditorMobileHeader />
-      <Stack
-        flexGrow={1}
-        direction="row"
-        sx={(theme) => ({
-          height: `calc(100vh - ${headerHeightCss})`,
-          overflow: "hidden",
+    <Box
+      sx={{
+        "--header-height": {
+          xs: "48px",
+          sm: "44px",
+          md: "44px",
+          lg: "44px",
+        },
 
-          [theme.breakpoints.up("md")]: {
-            height: "100vh",
-            overflow: "hidden",
-            paddingInlineEnd: isPropertiesPanelVisible
-              ? editorPropertiesPanelWidth.upMd
-              : 0,
-          },
-        })}
+        "--toolbar-height": {
+          xs: "50px",
+        },
+
+        "--canvas-brim-padding": {
+          xs: "0px",
+          // xs: "6px",
+          // sm: "10px",
+        },
+
+        "--previews-panel-width": {
+          xs: "180px",
+          sm: "200px",
+          md: "225px",
+          lg: "20vw",
+        },
+
+        "--properties-panel-width": {
+          xs: "auto",
+          sm: "360px",
+          md: "360px",
+          lg: "22vw",
+        },
+
+        "--canvas-max-width": {
+          md: "calc(100vw - (var(--canvas-brim-padding)*2) - var(--previews-panel-width) - var(--properties-panel-width))",
+        },
+      }}
+    >
+      <EditorHeader />
+
+      <Stack
+        direction="row"
+        flexGrow={1}
+        minWidth={0}
+        height={`calc(100vh - var(--header-height))`}
+        overflow={"hidden"}
       >
-        <EditorActivitiesPanel />
-        <Stack component="main" flexGrow={1}>
+        <EditorPreviewsPanel />
+        <Stack
+          component="main"
+          height={"100%"}
+          flexGrow={1}
+          minWidth={0}
+          overflow={"hidden"}
+        >
           <EditorToolBar />
           <EditorCanvas />
         </Stack>
         <EditorPropertiesPanel />
         <EditorPropertiesDrawerToggle />
       </Stack>
-    </>
+    </Box>
   );
 }
--- src/editor/constants.ts
+++
@@ -1,6 +0,0 @@
-// FIXME : Use mixins
-
-export const headerHeightCss = "135px";
-export const toolbarHeightCss = "50px";
-export const canvasPaddingYCss = "0px";
-export const canvasHeightCss = `calc(100vh - ${toolbarHeightCss} - ${canvasPaddingYCss})`;
