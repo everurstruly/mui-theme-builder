5d652d7667e153a1b501cac9fa6ad07bc5197f72
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-22 22:33:43 +0100

    refactor: remove unused component files

--- src/editor/Canvas/frames/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-
-export default function CanvasBoardFrames() {
-  return (
-    <Typography variant="h4" sx={{ p: 4, width: 600, height: 800 }}>
-      Canvas Board Frames
-    </Typography>
-  );
-}
