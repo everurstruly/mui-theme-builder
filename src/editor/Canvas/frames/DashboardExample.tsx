bcd0f779fb2ee979d32e8fa48ed458f86ab49493
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-10-23 01:55:14 +0100

    fix: remove deleted component

--- src/editor/Canvas/frames/DashboardExample.tsx
+++ src/editor/Canvas/frames/DashboardExample.tsx
@@ -1,20 +1,17 @@
-import FrameWrapper from "../FrameWrapper";
 import { Typography } from "@mui/material";
 
 export default function DashboardExample() {
   return (
-    <FrameWrapper>
-      <Typography
-        variant="h4"
-        sx={{
-          p: 6,
-          width: 1400,
-          height: 2000,
-          backgroundColor: "limegreen",
-        }}
-      >
-        I am a Dashboard (allegedly)
-      </Typography>
-    </FrameWrapper>
+    <Typography
+      variant="h4"
+      sx={{
+        p: 6,
+        width: 1400,
+        height: 2000,
+        backgroundColor: "limegreen",
+      }}
+    >
+      I am a Dashboard (allegedly)
+    </Typography>
   );
 }
