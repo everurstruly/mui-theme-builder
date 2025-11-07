23ee995203e6a13725e11670c254a0dff0e87905
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-11-07 02:33:09 +0100

    feat: add mouse over state management  editor panels

--- src/Editor/editorUiStore.ts
+++ src/Editor/editorUiStore.ts
@@ -10,11 +10,25 @@ export type EditorUiPanels =
 const useEditorUiStore = create(
   combine(
     {
+      mouseOverCanvas: false,
+      mouseOverPropertiesPanel: false,
       hasSavedChanges: true,
       hiddenPanels: ["properties.mobile", "explorer.mobile"] as EditorUiPanels[],
     },
 
     (set) => ({
+      setMouseOverPropertiesPanel: (isMouseOver: boolean) => {
+        set(() => ({
+          mouseOverPropertiesPanel: isMouseOver,
+        }));
+      },
+
+      setMouseOverCanvas: (isMouseOver: boolean) => {
+        set(() => ({
+          mouseOverCanvas: isMouseOver,
+        }));
+      },
+
       hidePanel: (panel: EditorUiPanels) => {
         set((state) => ({
           hiddenPanels: Array.from(new Set([...state.hiddenPanels, panel])),
