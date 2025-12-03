be9478134602a159f0ca466f40fd2949bee9efde
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-12-04 00:04:46 +0100

    fix: complete component api update

--- src/Editor/Design/Versions/VersionHistoryButton.tsx
+++ src/Editor/Design/Versions/VersionHistoryButton.tsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
 import { IconButton, Tooltip } from "@mui/material";
 import { History as HistoryIcon } from "@mui/icons-material";
-import { VersionHistoryDialog } from "./VersionHistoryDialog";
 import { useCurrent } from "../Current/useCurrent";
+import useEditor from "../../useEditor";
 
 export function VersionHistoryButton() {
-  const [dialogOpen, setDialogOpen] = useState(false);
   const currentDesignId = useCurrent((s) => s.persistenceSnapshotId);
+  const setDialogOpen = useEditor((s) => s.setVersionHistoryOpen);
 
   // Only show button if there's a saved design
   if (!currentDesignId) {
@@ -14,17 +13,10 @@ export function VersionHistoryButton() {
   }
 
   return (
-    <>
-      <Tooltip title="Version History">
-        <IconButton onClick={() => setDialogOpen(true)} size="small">
-          <HistoryIcon />
-        </IconButton>
-      </Tooltip>
-
-      <VersionHistoryDialog
-        open={dialogOpen}
-        onClose={() => setDialogOpen(false)}
-      />
-    </>
+    <Tooltip title="Version History">
+      <IconButton onClick={() => setDialogOpen(true)} size="small">
+        <HistoryIcon />
+      </IconButton>
+    </Tooltip>
   );
 }
