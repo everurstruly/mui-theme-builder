5d50b51eccadf3757dd5deaa5665eccf48122fd8
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-12-17 23:50:29 +0100

    fix: add language specific syntax hilighting

--- src/Editor/Design/Export/FileContent.tsx
+++ src/Editor/Design/Export/FileContent.tsx
@@ -1,15 +1,16 @@
 import useExport from "./useExport";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { fileExtensionToProgrammingLanguage } from "./fileExtensionToProgrammingLanguage";
 import { Box, type SxProps, type Theme } from "@mui/material";
 
 function FileContent({ sx }: { sx?: SxProps<Theme> }) {
   const { fileExtension, getExportCode } = useExport();
-
+  const language = fileExtensionToProgrammingLanguage(fileExtension) || fileExtension;
   return (
     <Box
       component={SyntaxHighlighter}
-      language={fileExtension}
+      language={language}
       style={vscDarkPlus}
       showLineNumbers
       sx={{
