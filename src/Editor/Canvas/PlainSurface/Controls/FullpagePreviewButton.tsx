a179daed5544b2f327e177f01691e60ff37cb78e
Oghenetefa Okotete <oghenetefa@gmail.com>
2025-12-18 01:33:31 +0100

    fix: out of sync state

--- src/Editor/Canvas/PlainSurface/Controls/FullpagePreviewButton.tsx
+++ src/Editor/Canvas/PlainSurface/Controls/FullpagePreviewButton.tsx
@@ -2,22 +2,15 @@ import FullscreenIcon from "@mui/icons-material/Fullscreen";
 import FullscreenExitIcon from "@mui/icons-material/FullscreenExit";
 import useEditor from "../../../useEditor";
 import { IconButton, Tooltip } from "@mui/material";
-import { useState } from "react";
 
 export default function FullpagePreviewButton() {
-  const [isFullpage, setIsFullpage] = useState(false);
-  const handleFullpage = useEditor((s) => s.hideCanvasSidebarPanels);
-  const exitFullpage = useEditor((s) => s.restoreCanvasSidebarPanels);
+  const isFullpage = useEditor((s) => s.isFullpage);
+  const toggleFullpage = useEditor((s) => s.toggleFullpage);
 
   const handleFullScreenToggle = () => {
-    if (isFullpage) {
-      exitFullpage();
-      setIsFullpage(false);
-    } else {
-      handleFullpage();
-      setIsFullpage(true);
-    }
+    toggleFullpage();
   };
+
   return (
     <Tooltip title={isFullpage ? "Exit fullscreen" : "Enter fullscreen"}>
       <IconButton
